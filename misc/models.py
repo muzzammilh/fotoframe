cad710a87407f06e83e846b54d673278ff0b7fac
Abdullah <110585986+abdullah956@users.noreply.github.com>
2023-07-15 02:23:45 -0700

    models.py

--- misc/models.py
+++ misc/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# creating base models for common fields created_at and updated_at to keep record of date and time
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-class Meta:
-    abstract = True
+
+    class Meta:
+        abstract = True
