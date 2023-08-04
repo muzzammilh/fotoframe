2bc49f1bcf32e7dc83096cfd9facae4596ac7731
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2023-08-04 15:57:04 +0500

    Update views.py

--- customer/views.py
+++ customer/views.py
@@ -1,27 +1,29 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login
 from .forms import SignUpForm
+
 def home(request):
-    return render(request, "customer/welcome.html", {"name": "Muzzammil", "title": "My web title"})
-def signup(request):
-    if request.method == 'POST':
+    return render(request, "customer/welcome.html", {"name": "Muzzamil", "title": "My Website Title"})
+
+def SignUpView(request):
+    if request.method == "POST":
         form = SignUpForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            #refresh the database
-            user.refresh_from_db()  
-            #save user
-            user.save()
-            raw_password = form.cleaned_data.get('password1')
-            #authenticate email password
-            user = authenticate(username=user.email, password=raw_password)
-            #login user
-            login(request, user)
-            # redirect user to home page
-            return redirect('home')
+            form.save()
+            return redirect("login")
     else:
         form = SignUpForm()
-    return render(request, 'customer/signup.html', {'form': form})
+    return render(request, "customer/signup.html", {"form": form})
 
+def LoginView(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
 
+        user = authenticate(email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, "customer/welcome.html", {"name": "Muzzamil", "title": "My Website Title"})
+    else:
+        return render(request, "customer/login.html")
 
