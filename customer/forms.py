85b25bcbd021a0185a3dc8647a42dcca1936e8b5
Abdullah <110585986+abdullah956@users.noreply.github.com>
2023-08-04 11:33:46 -0700

    created a user signup form

---
+++ customer/forms.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm
+
+from customer.models import AppUser
+#making a signupform
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = AppUser
+        fields = ('email', 'password1', 'password2', )
+
+
+
+
--- customer/urls.py
+++ customer/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("test/", views.index, name="index"),
+    path("test/", views.home, name="home"),
+    path('signup/', views.signup, name='signup'),
 ]
+
+
+
