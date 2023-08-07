962f119c496ec0337dddcc7cc1d4c89b9e7a9100
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2023-08-07 16:22:21 +0500

    Update forms.py

--- customer/forms.py
+++ customer/forms.py
@@ -1,12 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm
 
 from customer.models import AppUser
-#making a signupform
+
 class SignUpForm(UserCreationForm):
     class Meta:
         model = AppUser
-        fields = ('email', 'password1', 'password2', )
-
-
-
-        
+        fields = ('email', 'password1', 'password2')   
