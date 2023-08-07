5271e6caeb1bc3cf6ac46cd8e7f9fa6ef1fa5e3a
Abdullah <110585986+abdullah956@users.noreply.github.com>
2023-08-07 05:44:06 -0700

    Updated urls.py

--- customer/urls.py
+++ customer/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("test/", views.home, name="home"),
+    path("home/", views.home, name="home"),
     path('signup/', views.signup, name='signup'),
 ]
 
