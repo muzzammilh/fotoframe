d38afebcd64f35aee3e9b78a783db3909020d41e
Abdullah <110585986+abdullah956@users.noreply.github.com>
2023-08-07 16:03:13 -0700

    fixed the errors

--- customer/forms.py
+++ customer/forms.py
@@ -6,7 +6,3 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = AppUser
         fields = ('email', 'password1', 'password2', )
-
-
-
-
--- customer/urls.py
+++ customer/urls.py
@@ -3,9 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.SignUpView, name="signup"),
-    path("login/", views.LoginView, name="login"),
-    path("welcome/", views.welcome, name="welcome"),
+    path("test/", views.home, name="home"),
+    path('signup/', views.signup, name='signup'),
 ]
 
 
