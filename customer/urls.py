fe8bbd55dae1cea6314111894462a1ca252163af
Abdullah <110585986+abdullah956@users.noreply.github.com>
2023-08-29 01:21:29 -0700

    made loginpage and homepage image display (#22)

--- customer/urls.py
+++ customer/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("home/", views.home, name="home"),
     path('signup/', views.signup, name='signup'),
     path('upload/', views.upload_image, name='upload_image'),
+    path('',views.loginform, name='login'),
 ]
 
 
